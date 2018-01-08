6f13911c61f096e9ad76ce8debc4e88d36fc7f5b
fengxiaobin <rgfxb@qq.com>
2018-01-08 20:04:23 +0800

    感知机

--- machinelearningalogrithnm/Perception/perception.py
+++ machinelearningalogrithnm/Perception/perception.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import numpy as np
 
 # An example in that book, the training set and parameters' sizes are fixed
@@ -24,11 +22,7 @@ def perception_origin(X,Y,eta=1,w=(0,0),b=0):
     while True:
         for i in range(len(X)):
             # w*x+b<0为正确分类
-            print(i)
-            print(np.array(w),np.array(X[i]))
-            print(Y[i]*(sum(np.array(w)*np.array(X[i]))+b))
             if Y[i]*(sum(np.array(w)*np.array(X[i]))+b)<=0:
-                print(Y[i])
                 w=np.array(w)+eta*Y[i]*np.array(X[i])
                 b=b+Y[i]
                 break
@@ -36,6 +30,45 @@ def perception_origin(X,Y,eta=1,w=(0,0),b=0):
                 # 没有误分类点
                 return w,b
 
+def Gram(X):
+    '''
+    计算Gram矩阵G=[X[i]*X[j]]
+    :param X:
+    :return:
+    '''
+    n=len(X)
+    G=np.matrix(np.zeros((n,n)))
+    for i in range(len(X)):
+        for j in range(len(X)):
+            G[i,j]=sum(np.array(X[i])*np.array(X[j]))
+    return G
+
+def perception_dual(X,Y,eta=1,alpha=[0,0,0],b=0):
+    '''
+    感知机对偶形式
+    :param X: 训练集数据X
+    :param Y: 训练集标记Y
+    :param eta: 梯度下降步长
+    :return: w,b
+    '''
+    G=Gram(X)
+    while True:
+        for i in range(len(X)):
+            # w*x+b<0为正确分类
+            if Y[i] * (sum([alpha[j]*Y[j]*G[j,i] for j in range(len(X))])+b) <= 0:
+                alpha[i] = alpha[i]+1
+                b = b + Y[i]
+                break
+            if i == 2:
+                # 没有误分类点
+                w=sum([alpha[i]*Y[i]*np.array(X[i]) for i in range(len(X))])
+                b=sum([alpha[i]*Y[i] for i in range(len(X))])
+                print(alpha)
+                return w, b
+
+
 
 if __name__=="__main__":
     print(perception_origin(X,Y,eta=1,w=(0,0),b=0))
+    print(Gram(X))
+    print(perception_dual(X,Y,eta=1,alpha=[0,0,0],b=0))
